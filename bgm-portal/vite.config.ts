f3e8fafbb1025ca467d4a8e2f6c107d11b4cc93a
Phạm Hiếu Vinh <phvinh1968@gmail.com>
2026-02-09 11:46:12 +0700

    Configure build output directory and settings
    Set output directory for build and prevent emptying it.

--- bgm-portal/vite.config.ts
+++ bgm-portal/vite.config.ts
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/bgm-portal/",
+  build: {
+    outDir: "docs",     // 👈 RẤT QUAN TRỌNG
+    emptyOutDir: false  // 👈 KHÔNG XOÁ FILE CŨ
+  },
   plugins: [react()],
 });
